390211a8fe099fac1e886ab2505dfa285fd1530e
Fernando Cauper <fernando_cauper@hotmail.com>
2023-12-13 22:51:21 -0400

    first commit

---
+++ src/main/java/mxhtechnology/travelhelptripapp/TravelhelpTripAppApplication.java
@@ -0,0 +1,13 @@
+package mxhtechnology.travelhelptripapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelhelpTripAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelhelpTripAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mxhtechnology/travelhelptripapp/TravelhelpTripAppApplicationTests.java
@@ -0,0 +1,13 @@
+package mxhtechnology.travelhelptripapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelhelpTripAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
