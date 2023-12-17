b30ee038f4622c89f06c0ddbf82961253432963b
Fernando Cauper <fernando_cauper@hotmail.com>
2023-12-16 21:30:02 -0400

    feat: adding Localstack configurations

--- src/main/java/mxhtechnology/travelhelptripapp/repository/TripRepository.java
+++
@@ -1,8 +0,0 @@
-package mxhtechnology.travelhelptripapp.repository;
-
-import mxhtechnology.travelhelptripapp.entity.Trip;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-
-public interface TripRepository extends MongoRepository<Trip, String> {
-}
